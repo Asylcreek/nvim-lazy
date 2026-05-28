edeb7cf0208fa96f4c82fcca0991117c46cef580
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2026-05-28 11:58:35 +0100

    feat(keymaps): add context-aware log lines

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -110,7 +110,17 @@ end, { desc = "Close all buffers" })
 vim.keymap.set("n", "gct", "o<esc>VcTODO: x<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add todo comment below" })
 vim.keymap.set("n", "gcT", "O<esc>VcTODO: x<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add todo comment above" })
 vim.keymap.set("n", "<leader>yt", "<cmd>let @+ = expand('%:p')<cr>", { desc = "Copy absolute path" })
-vim.keymap.set("n", "<leader>cy", "oconsole.log();<esc>hi", { desc = "Type console.log below" })
-vim.keymap.set("n", "<leader>cY", "Oconsole.log();<esc>hi", { desc = "Type console.log above" })
+vim.keymap.set("n", "<leader>cy", function()
+  local ok, err = pcall(require("utils.context_line").insert, false)
+  if not ok then
+    vim.notify(err, vim.log.levels.ERROR)
+  end
+end, { desc = "Type context line below" })
+vim.keymap.set("n", "<leader>cY", function()
+  local ok, err = pcall(require("utils.context_line").insert, true)
+  if not ok then
+    vim.notify(err, vim.log.levels.ERROR)
+  end
+end, { desc = "Type context line above" })
 
 Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
---
+++ lua/utils/context_line.lua
@@ -0,0 +1,60 @@
+local M = {}
+
+local log_lines = {
+  c = 'printf("%s\\n", |);',
+  cpp = 'std::cout << | << "\\n";',
+  go = "fmt.Println(|)",
+  java = "System.out.println(|);",
+  javascript = "console.log(|);",
+  javascriptreact = "console.log(|);",
+  jsx = "console.log(|);",
+  lua = "print(|)",
+  php = "var_dump(|);",
+  python = "print(|)",
+  ruby = "puts |",
+  rust = 'println!("{:?}", |);',
+  sh = "echo |",
+  tsx = "console.log(|);",
+  typescript = "console.log(|);",
+  typescriptreact = "console.log(|);",
+}
+
+local function current_lang()
+  local ok, node = pcall(vim.treesitter.get_node, { ignore_injections = false })
+  if ok and node then
+    local range = { node:range() }
+    local parser_ok, parser = pcall(vim.treesitter.get_parser, 0)
+    local tree_ok, tree = parser_ok and pcall(parser.language_for_range, parser, range)
+
+    if tree_ok and tree then
+      return tree:lang()
+    end
+  end
+
+  return vim.treesitter.language.get_lang(vim.bo.filetype) or vim.bo.filetype
+end
+
+local function snippet()
+  local lang = current_lang()
+  local line = log_lines[lang]
+
+  if line then
+    return line
+  end
+
+  error(("No log line configured for %q"):format(lang), 0)
+end
+
+function M.insert(above)
+  local line = snippet()
+  local marker = line:find("|", 1, true) or (#line + 1)
+  local text = line:gsub("|", "", 1)
+  local current_row = vim.api.nvim_win_get_cursor(0)[1]
+  local insert_row = above and current_row - 1 or current_row
+
+  vim.api.nvim_buf_set_lines(0, insert_row, insert_row, false, { text })
+  vim.api.nvim_win_set_cursor(0, { insert_row + 1, marker - 1 })
+  vim.cmd.startinsert()
+end
+
+return M
